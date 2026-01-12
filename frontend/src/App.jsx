8371705b1d5368e1a10e22d412915433d4915d85
Jason <jasonchawsuan@gmail.com>
2026-01-12 09:33:43 +0700

    feat: Add navbar, tailwindcss, and routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/NavBar'
+import {Routes, Route, Navigate} from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
 
 function App() {
 
   return (
     <>
-      <div>
-        <h1>This is dashboard.</h1>
-      </div>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Navigate to="/dashboard" replace />} />
+        <Route path='/dashboard' element={<Dashboard/>}></Route>
+      </Routes>
     </>
   )
 }
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+    return (
+        <nav className="fixed top-0 left-0 w-full h-16 bg-gray-900 text-white flex items-center z-50 px-6">
+            <h3>Home</h3>
+        </nav>
+    )
+}
